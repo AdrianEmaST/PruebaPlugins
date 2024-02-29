16aa2c1f34fb46befaba27dc018005990ad50506
AdrianEmaST <desarrolloadrianjr@gmail.com>
2024-02-28 23:59:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Person from './Component/Persons/Person'
+
+function App() {
+
+
+  return (
+
+    <>
+    
+      <Person />
+    
+    </>
+
+
+  )
+}
+
+export default App
